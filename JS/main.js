3c3881682daad751c5da0dfc3d1a8b15d4a872d2
Awaab Abdelbagi <92119191+2waab@users.noreply.github.com>
2022-08-17 17:16:35 +0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,20 @@
 let toggler = document.querySelector(".toggle");
 let nav = document.querySelector("nav");
 let close = document.querySelector(".close");
-let link = document.querySelectorAll(".link");
+let link = document.querySelectorAll("nav a");
 
 toggler.onclick = () => {
     nav.classList.add("open");
 };
 
-link.onclick = () => {
+close.onclick = () => {
     nav.classList.remove("open");
 };
 
-close.onclick = () => {
-    nav.classList.remove("open");
+for (let i = 0; i <= link.length; i++) {
+    link[i].onclick = () => {
+        nav.classList.remove("open");
+    };
 };
 
 document.onkeyup = (e) => {
